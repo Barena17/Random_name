cd62aae9c1a925ec954e238ee239569558bae89f
Barena17 <92177275+Barena17@users.noreply.github.com>
2021-11-10 09:55:18 -0800

    Update Random name 2.0.py

--- Random name 2.0.py
+++ Random name 2.0.py
@@ -3,20 +3,20 @@ import random
 print("Hello there!")
 firstname = input("What's Your name?")
 
-# input names in the fields. 
+# New Class Name Generator. Insert more name if needed.  
 
-first = ["Jim", "Joe", "john", "Bob", "Billy"]
-last = ["Capone", "Dahlmer", "Mansion", "Jones", "Trump"]
+first = ["CARPE SCHEME", "TEAM BONSI", "DNS"]
 namebank = []
 
 while True:
-    name = input("Do you want to generate a name? [y/n]:")
+    name = input("Do you want to generate a new name? [y/n]:")
     if name.upper() == "Y":
-        rand = random.randint(0, 4)
-        randname = first[rand] + " " + last[rand]
+        rand = random.randint(0, 3)
+        randname = first[rand]
         namebank.append(randname)
         print(f"Your new name is {randname}\n")
         continue
     else:
         print("Thank you for creating a new name")
     break
+
